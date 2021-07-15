1c42474dfbb880c84c324f83534a23e95b48bf7a
havanadriana <83068063+havanadriana@users.noreply.github.com>
2021-07-15 23:11:08 +0200

    holaaaaaaa

--- .eleventy.js
+++ .eleventy.js
@@ -12,21 +12,3 @@ module.exports = function(eleventyConfig) {
   }
 }
 
-function slideToggle(myEvent) {
-  myEvent.preventDefault();
-
-  var myTarget = this.getAttribute('href');
-  var theBlock = document.querySelector(myTarget);
-  var finalHeight = theBlock.querySelector('.content-block').clientHeight;
-  if (theBlock.classList.contains('is-open')) {
-    theBlock.classList.remove('is-open');
-    theBlock.style.maxHeight = 0;
-  } else {
-    theBlock.classList.add('is-open');
-    theBlock.style.maxHeight = finalHeight + 'px';
-  }
-}
-
-document.querySelectorAll('.trigger').forEach(function(myElement) {
-  myElement.addEventListener('click', slideToggle);
-});
--- src/scripts/eventos.js
+++ src/scripts/eventos.js
@@ -3,20 +3,20 @@ import list from './modules/list'
 console.log(`Hello ${list[0]}`)
 
 function slideToggle(myEvent) {
-  myEvent.preventDefault();
-
-  var myTarget = this.getAttribute('href');
-  var theBlock = document.querySelector(myTarget);
-  var finalHeight = theBlock.querySelector('.content-block').clientHeight;
-  if (theBlock.classList.contains('is-open')) {
-    theBlock.classList.remove('is-open');
-    theBlock.style.maxHeight = 0;
-  } else {
-    theBlock.classList.add('is-open');
-    theBlock.style.maxHeight = finalHeight + 'px';
+    myEvent.preventDefault();
+  
+    var myTarget = this.getAttribute('href');
+    var theBlock = document.querySelector(myTarget);
+    var finalHeight = theBlock.querySelector('.content-block').clientHeight;
+    if (theBlock.classList.contains('is-open')) {
+      theBlock.classList.remove('is-open');
+      theBlock.style.maxHeight = 0;
+    } else {
+      theBlock.classList.add('is-open');
+      theBlock.style.maxHeight = finalHeight + 'px';
+    }
   }
-}
-
-document.querySelectorAll('.trigger').forEach(function(myElement) {
-  myElement.addEventListener('click', slideToggle);
-});
+  
+  document.querySelectorAll('.trigger').forEach(function(myElement) {
+    myElement.addEventListener('click', slideToggle);
+  });
